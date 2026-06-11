53dd92fd11435c94a58b333e8616421f18746e3a
Peter Epp <peter.epp@icloud.com>
2026-06-10 22:03:10 -0600

    chore: update for coding standards

--- api/src/routes.php
+++ api/src/routes.php
@@ -7,12 +7,13 @@ use App\Handlers\ManifestHandler;
 use App\Handlers\PageHandler;
 use App\Middleware\CorsMiddleware;
 use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface;
 
 /** @var App $app */
 
 $app->add(CorsMiddleware::class);
 
-$app->group('/api', function ($group) {
+$app->group('/api', function (RouteCollectorProxyInterface $group): void {
     $group->get('/page/{page}', PageHandler::class);
     $group->get('/manifest/{name}', ManifestHandler::class);
     $group->get('/content/{type}/{slug}', ContentHandler::class);
