7c8754269e291619e9c563fd5466be3b9b0857f2
Peter Epp <peter.epp@icloud.com>
2026-06-13 19:26:57 -0600

    chore: add some content with a link in for testing

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,6 +8,11 @@ import ContentCard from '@/components/ui/ContentCard.vue'
     <SectionHeading>Peter Epp</SectionHeading>
     <ContentCard>
       <p>Hero section, featured work, featured articles, and skills will live here.</p>
+      <p>
+        This is a paragraph with a link to
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_positioned_layout/Understanding_z-index">an MDN article about z-index and stacking contexts in CSS positioned layout</a>
+        that is long enough to wrap onto the next line mid-link.
+      </p>
     </ContentCard>
   </div>
 </template>
